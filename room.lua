e6de47532476d8d92d17cbd0669b1a5a9d8b7eb3
Brooks Patton <scgryphon@sbcglobal.net>
2017-04-22 13:56:23 -0600

    we can limit how many balls are created now

--- room.lua
+++ room.lua
@@ -13,9 +13,12 @@ function Room.new()
   t.location = Vector.new(0, 0)
   t.width = width
   t.height = height
-  t.ball = Ball.new()
+  t.balls = {}
   t.gravity = Vector.new(0, 30)
   t.player = Player.new()
+  t.timeBallLastAdded = love.timer.getTime()
+  t.maxBalls = 5
+  t.ballsCreated = 0
 
   return t
 end
@@ -24,16 +27,54 @@ function Room:draw()
   love.graphics.setColor(255, 255, 255)
   love.graphics.rectangle('line', self.location.x, self.location.y, self.width, self.height)
 
-  self.ball:draw()
+  for i, ball in ipairs(self.balls) do
+    ball:draw()
+  end
 
   self.player:draw()
 end
 
 function Room:update(dt)
-  self.ball:applyForce(self.gravity * dt)
-  self.ball:update()
-  self.ball:checkEdges()
+  for i, ball in ipairs(self.balls) do
+    ball:applyForce(self.gravity * dt)
+    ball:update()
+    ball:checkEdges()
+
+    if self.player.bullet then
+      if self:didBulletHitBall(ball, self.player.bullet) then
+        ball = nil
+      end
+    end
+  end
+
+  if self:shouldAddBall() then
+    table.insert(self.balls, Ball.new())
+    self.ballsCreated = self.ballsCreated + 1
+  end
+
   self.player:update(dt)
+end
+
+function Room:didBulletHitBall(ball, bullet)
+  local dist = ball.location:dist(bullet.location)
+
+  if dist < ball.radius then
+    return true
+  end
+
+  return false
+end
+
+function Room:shouldAddBall()
+  local newTime = love.timer.getTime()
+  local delta = newTime - self.timeBallLastAdded
+
+  if delta >= 1 and self.ballsCreated < self.maxBalls then
+    self.timeBallLastAdded = newTime
+    return true
+  end
+
+  return false
 end
 
 return Room
