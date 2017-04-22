0893f5a22256262dc0c85dbc3dee7554ee8f32d7
Brooks Patton <scgryphon@sbcglobal.net>
2017-04-22 13:17:22 -0600

    bullets fire now from the player

---
+++ bullet.lua
@@ -0,0 +1,34 @@
+local Vector = require('./vector')
+
+local Bullet = {}
+Bullet.__index = Bullet
+
+function Bullet.new(location)
+  local t = {}
+  setmetatable(t, Bullet)
+
+  t.acceleration = Vector.new(0, 0)
+  t.location = location
+  t.velocity = Vector.new(0, 0)
+  t.speed = 8
+  t.size = 2
+
+  return t
+end
+
+function Bullet:draw()
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.circle('fill', self.location.x, self.location.y, self.size)
+end
+
+function Bullet:applyForce(force)
+  self.acceleration = self.acceleration + force
+end
+
+function Bullet:update(dt)
+  self.velocity = self.velocity + self.acceleration * dt
+  self.location = self.location + self.velocity * self.speed
+  self.acceleration = self.acceleration * 0
+end
+
+return Bullet
--- player.lua
+++ player.lua
@@ -1,4 +1,5 @@
 local Vector = require('./vector')
+local Bullet = require('./bullet')
 
 local Player = {}
 Player.__index = Player
@@ -14,6 +15,7 @@ function Player.new()
   t.velocity = Vector.new(0, 0)
   t.acceleration = Vector.new(0, 0)
   t.friction = 0.90
+  t.bullet = nil
 
   return t
 end
@@ -21,11 +23,16 @@ end
 function Player:draw()
   love.graphics.setColor(255, 255, 255)
   love.graphics.rectangle('fill', self.location.x, self.location.y, self.width, self.height)
+
+  if self.bullet then
+    self.bullet:draw()
+  end
 end
 
 function Player:update(dt)
   local isLeftDown = love.keyboard.isScancodeDown('left')
   local isRightDown = love.keyboard.isScancodeDown('right')
+  local isMouseDown = love.mouse.isDown(1)
 
   if isLeftDown then
     local move = Vector.new(self.speed * -1, 0)
@@ -35,14 +42,49 @@ function Player:update(dt)
     self:applyForce(move * dt)
   end
 
+  if isMouseDown and not self.bullet then
+    local location = self.location:clone()
+    local mouseLocation = Vector.new(love.mouse.getX(), love.mouse.getY())
+    local direction = mouseLocation - location
+    direction = direction:normalized() * 100
+
+    self.bullet = Bullet.new(location)
+
+    self.bullet:applyForce(direction)
+  elseif self.bullet then
+    if self:isBulletOffScreen() then
+      self.bullet = nil
+    end
+  end
+
   self.velocity = self.velocity + self.acceleration
   self.location = self.location + self.velocity
   self.acceleration = self.acceleration * 0
   self.velocity = self.velocity * self.friction
+
+  if self.bullet then
+    self.bullet:update(dt)
+  end
 end
 
 function Player:applyForce(force)
   self.acceleration = self.acceleration + force
+end
+
+function Player:isBulletOffScreen()
+  local x = self.bullet.location.x
+  local y = self.bullet.location.y
+  local result = false
+
+  if x > width or x < 0 then
+    result = true
+  end
+
+  if y > height or y < 0 then
+    result = true
+  end
+
+  return result
 end
 
 return Player
