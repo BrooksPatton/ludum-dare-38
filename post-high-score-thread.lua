1babfab3b5d7479d72a274e48c9d980968c04a1f
Brooks Patton <scgryphon@sbcglobal.net>
2017-04-24 20:52:05 -0600

    :bug: fixed a security hole in the server

--- post-high-score-thread.lua
+++ post-high-score-thread.lua
@@ -2,8 +2,9 @@ local http = require('socket.http')
 
 local channel = love.thread.getChannel('postScore')
 local score = channel:pop()
-local body = 'score=' .. score
+local body = 'score=' .. score .. '&code=supersecretcode'
 
-http.request('https://conquest-game.herokuapp.com/api/v1/high-score', body)
+--http.request('https://conquest-game.herokuapp.com/api/v1/high-score', body)
+http.request('http://localhost:3000/api/v1/high-score', body)
 
 love.event.quit(0)
