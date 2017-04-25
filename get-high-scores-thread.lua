81a07685ea8af02718712c851246a9cb67cc2970
Brooks Patton <scgryphon@sbcglobal.net>
2017-04-24 21:26:29 -0600

    :bug:

--- get-high-scores-thread.lua
+++ get-high-scores-thread.lua
@@ -1,7 +1,7 @@
 local http = require('socket.http')
 
-  --local result = http.request('https://conquest-game.herokuapp.com/api/v1/high-score')
-  local result = http.request('http://localhost:3000/api/v1/high-score')
+  local result = http.request('https://conquest-game.herokuapp.com/api/v1/high-score')
+  --local result = http.request('http://localhost:3000/api/v1/high-score')
   local channel = love.thread.getChannel('getAllScores')
   channel:push(result)
 love.event.quit(0)
--- post-high-score-thread.lua
+++ post-high-score-thread.lua
@@ -2,9 +2,9 @@ local http = require('socket.http')
 
 local channel = love.thread.getChannel('postScore')
 local score = channel:pop()
-local body = 'score=' .. score .. '&code=supersecretcode'
+local body = 'score=' .. score .. '&code=uywfplgjrstnhxnrbeirsndjuwyflj87w45ge3nhdneistvers'
 
---http.request('https://conquest-game.herokuapp.com/api/v1/high-score', body)
-http.request('http://localhost:3000/api/v1/high-score', body)
+http.request('https://conquest-game.herokuapp.com/api/v1/high-score', body)
+--http.request('http://localhost:3000/api/v1/high-score', body)
 
 love.event.quit(0)
